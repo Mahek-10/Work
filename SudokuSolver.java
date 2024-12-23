fb758221d3d207f69470ef392ad7b31602762f32
Mahek-10 <mahekjunedi10@gmail.com>
2024-12-23 20:00:49 +0530

    Update SudokuSolver.java

--- SudokuSolver.java
+++ SudokuSolver.java
@@ -1,87 +1,87 @@
-public static void main(String[] args) {
-    int[][] board = new int[SIZE][SIZE];
-    Scanner scanner = new Scanner(System.in);
+public class SudokuSolver {
+    private static final int SIZE = 9;
 
-    System.out.println("Enter the Sudoku puzzle (use 0 for empty cells):");
-    for (int i = 0; i < SIZE; i++) {
-        for (int j = 0; j < SIZE; j++) {
-            board[i][j] = scanner.nextInt();
+    public static void main(String[] args) {
+        int[][] board = {
+            {5, 3, 0, 0, 7, 0, 0, 0, 0},
+            {6, 0, 0, 1, 9, 5, 0, 0, 0},
+            {0, 9, 8, 0, 0, 0, 0, 6, 0},
+            {8, 0, 0, 0, 6, 0, 0, 0, 3},
+            {4, 0, 0, 8, 0, 3, 0, 0, 1},
+            {7, 0, 0, 0, 2, 0, 0, 0, 6},
+            {0, 6, 0, 0, 0, 0, 2, 8, 0},
+            {0, 0, 0, 4, 1, 9, 0, 0, 5},
+            {0, 0, 0, 0, 8, 0, 0, 7, 9}
+        };
+
+        if (solveSudoku(board)) {
+            printBoard(board);
+        } else {
+            System.out.println("No solution exists");
         }
     }
 
-    if (solveSudoku(board)) {
-        System.out.println("Solved Sudoku:");
-        printBoard(board);
-    } else {
-        System.out.println("No solution exists.");
-    }
-
-    scanner.close();
-}
-
-// Function to solve the Sudoku puzzle
-private static boolean solveSudoku(int[][] board) {
-    for (int row = 0; row < SIZE; row++) {
-        for (int col = 0; col < SIZE; col++) {
-            if (board[row][col] == 0) { // Find an empty cell
-                for (int num = 1; num <= SIZE; num++) { // Try numbers 1-9
-                    if (isSafe(board, row, col, num)) {
-                        board[row][col] = num; // Place the number
-
-                        if (solveSudoku(board)) { // Recursively try to solve
-                            return true;
+    private static boolean solveSudoku(int[][] board) {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (board[i][j] == 0) {
+                    for (int num = 1; num <= SIZE; num++) {
+                        if (isValid(board, i, j, num)) {
+                            board[i][j] = num;
+                            if (solveSudoku(board)) {
+                                return true;
+                            }
+                            board[i][j] = 0;
                         }
-
-                        board[row][col] = 0; // Backtrack
                     }
+                    return false;
                 }
-                return false; // No valid number found, trigger backtracking
             }
         }
-    }
-    return true; // Solved
-}
-
-// Check if it's safe to place a number in the given cell
-private static boolean isSafe(int[][] board, int row, int col, int num) {
-    // Check the row
-    for (int x = 0; x < SIZE; x++) {
-        if (board[row][x] == num) {
-            return false;
-        }
+        return true;
     }
 
-    // Check the column
-    for (int x = 0; x < SIZE; x++) {
-        if (board[x][col] == num) {
-            return false;
-        }
-    }
-
-    // Check the 3x3 box
-    int startRow = row - row % 3;
-    int startCol = col - col % 3;
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            if (board[i + startRow][j + startCol] == num) {
+    private static boolean isValid(int[][] board, int row, int col, int num) {
+        // Check the row
+        for (int i = 0; i < SIZE; i++) {
+            if (board[row][i] == num) {
                 return false;
             }
         }
+
+        // Check the column
+        for (int i = 0; i < SIZE; i++) {
+            if (board[i][col] == num) {
+                return false;
+            }
+        }
+
+        // Check the box
+        int boxRow = row - row % 3;
+        int boxCol = col - col % 3;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[boxRow + i][boxCol + j] == num) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
 
-    return true; // Safe to place the number
-}
-
-// Print the Sudoku board
-private static void printBoard(int[][] board) {
-    for (int r = 0; r < SIZE; r++) {
-        for (int d = 0; d < SIZE; d++) {
-            System.out.print(board[r][d] + " ");
-        }
-        System.out.print("\n");
-
-        if ((r + 1) % 3 == 0) {
-            System.out.print("\n");
+    private static void printBoard(int[][] board) {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                System.out.print(board[i][j] + " ");
+                if ((j + 1) % 3 == 0 && j < 8) {
+                    System.out.print("| ");
+                }
+            }
+            System.out.println();
+            if ((i + 1) % 3 == 0 && i < 8) {
+                System.out.println("--------+---------+--------");
+            }
         }
     }
 }
