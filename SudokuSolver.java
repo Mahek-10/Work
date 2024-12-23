5ddc12dc61707255923915e8c16cfa1068facc70
Mahek-10 <mahekjunedi10@gmail.com>
2024-12-23 19:14:07 +0530

    Update SudokuSolver.java

---
+++ SudokuSolver.java
@@ -0,0 +1,87 @@
+public class SudokuSolver {
+    private static final int SIZE = 9;
+
+    public static void main(String[] args) {
+        int[][] board = {
+            {5, 3, 0, 0, 7, 0, 0, 0, 0},
+            {6, 0, 0, 1, 9, 5, 0, 0, 0},
+            {0, 9, 8, 0, 0, 0, 0, 6, 0},
+            {8, 0, 0, 0, 6, 0, 0, 0, 3},
+            {4, 0, 0, 8, 0, 3, 0, 0, 1},
+            {7, 0, 0, 0, 2, 0, 0, 0, 6},
+            {0, 6, 0, 0, 0, 0, 2, 8, 0},
+            {0, 0, 0, 4, 1, 9, 0, 0, 5},
+            {0, 0, 0, 0, 8, 0, 0, 7, 9}
+        };
+
+        if (solveSudoku(board)) {
+            printBoard(board);
+        } else {
+            System.out.println("No solution exists.");
+        }
+    }
+
+    private static boolean solveSudoku(int[][] board) {
+        for (int row = 0; row < SIZE; row++) {
+            for (int col = 0; col < SIZE; col++) {
+                if (board[row][col] == 0) { // Find an empty cell
+                    for (int num = 1; num <= SIZE; num++) { // Try numbers 1-9
+                        if (isSafe(board, row, col, num)) {
+                            board[row][col] = num; // Place the number
+
+                            if (solveSudoku(board)) { // Recursively try to solve
+                                return true;
+                            }
+
+                            board[row][col] = 0; // Backtrack
+                        }
+                    }
+                    return false; // No valid number found, trigger backtracking
+                }
+            }
+        }
+        return true; // Solved
+    }
+
+    private static boolean isSafe(int[][] board, int row, int col, int num) {
+        // Check row
+        for (int x = 0; x < SIZE; x++) {
+            if (board[row][x] == num) {
+                return false;
+            }
+        }
+
+        // Check column
+        for (int x = 0; x < SIZE; x++) {
+            if (board[x][col] == num) {
+                return false;
+            }
+        }
+
+        // Check 3x3 box
+        int startRow = row - row % 3;
+        int startCol = col - col % 3;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i + startRow][j + startCol] == num) {
+                    return false;
+                }
+            }
+        }
+
+        return true; // Safe to place the number
+    }
+
+    private static void printBoard(int[][] board) {
+        for (int r = 0; r < SIZE; r++) {
+            for (int d = 0; d < SIZE; d++) {
+                System.out.print(board[r][d] + " ");
+            }
+            System.out.print("\n");
+
+            if ((r + 1) % 3 == 0) {
+                System.out.print("");
+            }
+        }
+    }
+}
